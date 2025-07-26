20e3bf43931f02e909aa935c4a19ac6a16e4765b
JeffersonNayron <jeffersonnayron.santos@outlook.com>
2025-07-26 04:34:35 -0300

    Corrige porta para produção no Railway

--- server.js
+++ server.js
@@ -76,6 +76,9 @@ app.get('/api/version', (req, res) => {
   res.json({ version: '1.0.0' }); // coloque a versão que quiser aqui
 });
 
-app.listen(3000, () => {
-  console.log("Servidor rodando em http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
+
