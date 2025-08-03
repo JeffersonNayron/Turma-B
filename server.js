4aca4911744149840979c577963e04bc6485cbf4
JeffersonNayron <jeffersonnayron.santos@outlook.com>
2025-08-03 02:07:02 -0300

    server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const moment = require('moment-timezone');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
